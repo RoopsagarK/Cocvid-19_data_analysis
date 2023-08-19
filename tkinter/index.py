74728f849ea00e4f5e73a90a4969460efbb05572
vanamuthuV <109649687+vanamuthuV@users.noreply.github.com>
2023-08-19 15:22:44 +0530

    Fourth Commit

--- tkinter/index.py
+++ tkinter/index.py
@@ -1,16 +1,14 @@
 from tkinter import *
 import matplotlib.pyplot as plt
 import pandas as pd
+import import_ipynb
+import covid_analysis.ipynb
 from PIL import ImageTk, Image
 
 root = Tk()
 root.title('Covid-19 Data Analyzer')
 root.iconbitmap('tkinter/icon/covid-19.ico')
 root.configure(bg='#272829')
-
-MyDateList = []
-for i in range(32) :
-    MyDateList.append(i)
 
 SearchFrame = LabelFrame(root, width=100,height=50)
 SearchFrame.grid(row=0,column=0,columnspan=3,padx=5,pady=5)
@@ -30,24 +28,18 @@ DataFrame.grid(row=2, column=0,padx=15,pady=15)
 DataMapLabel = Label(DataFrame,text='Map',width=45,height=15,bg='lightblue',borderwidth=5)
 DataMapLabel.grid(row=0,column=0,rowspan=2,padx=40)
 
-CaseValue = StringVar()
-MonthValue = StringVar()
-DateValue = IntVar()
+CategoryValue1 = StringVar()
+CategoryValue2 = StringVar()
 
-Cases = OptionMenu(DataFrame, CaseValue, 'Death Rate', 'Mortality Rate', 'Incident Rate', 'Daily Change Rate')
+Category = ['Confirmed', 'Deaths', 'Recovered', 'Active', 'New-Cases', 'New-Deaths', 'New-Recovered']
+
+Cases = OptionMenu(DataFrame, CategoryValue1, *Category)
 Cases.grid(row=0 , column=1,padx=5, pady=5)
-CaseValue.set('Select Category')
+CategoryValue1.set('Select Category')
 
-Months = ['January', 'Febrauary', 'March', 'April', 'May', 'June', 'July']
-
-
-MonthValue.set('Select Month')
-Month = OptionMenu(DataFrame,MonthValue, *Months) 
-Month.grid(row=0, column=2, padx=5,pady=5)
-
-DateValue.set('Select Date')
-Date = OptionMenu(DataFrame, DateValue, *MyDateList) 
+Date = OptionMenu(DataFrame, CategoryValue2, *Category) 
 Date.grid(row=0, column=3, padx=5, pady=5)
+CategoryValue2.set('Select Category')
 
 Graph = Label(DataFrame, text='Graph', width=55,height=13,bg='pink',borderwidth=5)
 Graph.grid(row=1,column=1,columnspan=3,padx=15,pady=12)
